a372cf3f3ec245e1bae39ab3f8be96ef707d8b66
Ian Hayhurst <root@gbjhccx00.eame.syngenta.org>
2020-07-17 10:05:00 +0100

    separated out tablestyling as function and removed wildcard imports

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 # I.M. Hayhurst 2020 06 30
 
+import os
 from flask import Flask, render_template
 import flask
 import datetime
@@ -7,10 +8,10 @@ import pandas as pd
 
 from bs4 import BeautifulSoup
 # Application imports
-from HPCapps.uqueue import *
-from HPCapps.patching_load import *
-from HPCapps.inventory_load_host import *
-from HPCapps.structures_api import *
+import HPCapps.uqueue
+import HPCapps.patching_load
+import HPCapps.inventory_load_host
+import HPCapps.structures_api
 
 
 app = Flask(__name__)
@@ -23,20 +24,26 @@ def index():
 
 @app.route('/queue')
 def queue():
-    gridjobs = getGrid()
-    jobhistory = getGridHistory()
+    gridjobs = HPCapps.uqueue.getGrid()
+    jobhistory =HPCapps.uqueue.getGridHistory()
     return render_template('queue.html', gridjobs=gridjobs, jobhistory=jobhistory)
 
 @app.route('/queue-xml') # No menu link yet as experimental
 def queuexml():
-    gridjobs = getGridXML()
+    gridjobs =HPCapps.uqueue.getGridXML()
     soup = BeautifulSoup(gridjobs, 'xml')
     return render_template('queue.html', gridjobs=gridjobs, jobhistory=None)
 
 
 @app.route('/showpatching')
 def patching():
-    df = getPatching()
+    df = HPCapps.patching_load.getPatching()
+    styledPatchingTable = applyTableStyle(df)
+    return render_template('patching.html', data=styledPatchingTable)
+
+
+def applyTableStyle(df):
+
     styles = [
               hover(),
               dict(selector="th", props=[("font-size", "110%"),
@@ -53,9 +60,7 @@ def patching():
                      .format({'hostname': make_clickable})
                      .set_precision(0)
                      .render())
-
-    return render_template('patching.html', data=patchingStyle)
-
+    return patchingStyle
 
 def hover(hover_color="#000033"):
     return dict(selector="tr:hover",
@@ -71,7 +76,7 @@ def colorGrade(val):
     patchingCritical = 60
     patchingUrgent = 50
     if (val >= patchingCritical):
-        color = 'red' 
+        color = 'red'
     elif (val >= patchingUrgent):
         color = 'orange'
     else:
@@ -85,14 +90,14 @@ def make_clickable(val):
 @app.route('/inventory/<hostname>', methods=['GET', 'POST'])
 def inventory_host(hostname):
     para1 = hostname
-    d = getInventoryHost(hostname)
+    d = HPCapps.inventory_load_host.getInventoryHost(hostname)
     #df = pd.json_normalize(d['contacts'], errors='ignore')
     df = pd.json_normalize(d, errors='ignore')
     return render_template("inventory_host.html", para1=para1, data=df.to_html())
 
 @app.route('/structures')
 def structuresapi():
-    data = getStructuresApi() 
+    data =HPCapps.structures_api.getStructuresApi()
     df = pd.json_normalize(data, errors='ignore')
     return render_template('inventory_host.html', para1='wibble', data=df.to_html())
 

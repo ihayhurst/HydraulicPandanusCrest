c6343fa58658e24a3d27a61baea4fad10c463ecc
ihayhurst <29225030+ihayhurst@users.noreply.github.com>
2020-12-30 18:43:51 +0000

    add task update for progress bar

--- flask/app/HPCapps/inventory_load.py
+++ flask/app/HPCapps/inventory_load.py
@@ -7,6 +7,8 @@ import pandas as pd
 import json
 import glob
 from datetime import datetime as dt
+from celery import current_task
+
 
 import gevent
 
@@ -17,10 +19,21 @@ def getInventory():
     Fetch list of dataframes and pass to concatenation
     Return dataframe.
     """
+    NTOTAL = 4
+    update_task(0.5, NTOTAL)
     li = loadDataFiles()
+    update_task(1, NTOTAL)
     df = concatToDataframe(li)
+    update_task(2, NTOTAL)
     df = processDataframe(df)
+    update_task(3, NTOTAL)
     return df
+
+
+def update_task(progress, NTOTAL):
+    current_task.update_state(state='PROGRESS',
+               meta={'current':progress,'total':NTOTAL})
+    return 999
 
 
 def loadDataFiles():

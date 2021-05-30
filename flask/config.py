019a804d577464a78aebedbeb210d2f308ddce59
ihayhurst <29225030+ihayhurst@users.noreply.github.com>
2021-05-30 20:41:56 +0100

    minimal config item for repo

--- flask/config.py
+++ flask/config.py
@@ -2,5 +2,7 @@
 
 class Config(object):
     APP_NAME = "HPC-homedev"
+    LDAP_PORT = "389"
+    SECRET_KEY = 'justsoitworksoutofgit-overridewithinstanceconf'
 class Production(Config):
     APP_NAME = "HPC-homeProd"

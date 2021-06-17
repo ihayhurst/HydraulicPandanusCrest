edfed02e26c763bb3f6282afb78097de0caee6a6
ihayhurst <29225030+ihayhurst@users.noreply.github.com>
2021-06-17 21:56:27 +0100

    improve adherance to factory pattern

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,8 +1,6 @@
 from flask import Flask
 from flask_mail import Mail
 #from .website.views import website
-from .api.views import api_pages
-from .errors import errors
 
 # from extensidbons import *
 # Set Globals
@@ -14,10 +12,16 @@ def create_app():
     app.config.from_object("config.Production")
     # Override config with instance if present
     app.config.from_pyfile("flask.cfg", silent=True)
+    
     #Initialise Plugins
     mail.init_app(app)
-    from .website.views import website
+
     with app.app_context():
+
+        from .website.views import website
+        from .api.views import api_pages
+        from .errors import errors
+
         app.register_blueprint(website, url_prefix="/")
         app.register_blueprint(api_pages, url_prefix="/api")
         app.register_blueprint(errors)
